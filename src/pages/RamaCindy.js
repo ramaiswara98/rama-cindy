1773b23421aab58f5594e32f45558cb6a0a97cb4
ramaiswara98 <ramaiswara098@gmail.com>
2023-11-28 18:53:30 +0800

    tambahkan menu buka undangan

---
+++ src/pages/RamaCindy.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+import './RamaCindy.css'
+
+function RamaCindy() {
+
+  return (
+    <div className="cover-undangan" >
+      <div className='the-wedding-of-container'>
+        <p className='the-wedding-of-text'>The Wedding Of</p>
+      </div>
+      <div className='nama-mempelai-container'>
+        <div className='nama-mempelai-1'>
+        <p className='nama-mempelai-text'>Rama</p>
+        </div>
+        <div className='nama-mempelai-dan'>
+        <p className='nama-mempelai-text'>&</p>
+        </div>
+        <div className='nama-mempelai-2'>
+        <p className='nama-mempelai-text'>Cindy</p>
+        </div>
+      </div>
+      <div>
+        <p>Kepada Yth</p>
+      </div>
+      <div>
+        <p>Ahmad Heriyawan</p>
+      </div>
+      <div>
+        <p>Mohon Maaf Apabila Ada Kesalahan Penulisan Nama/Gelar</p>
+      </div>
+      <div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default RamaCindy
