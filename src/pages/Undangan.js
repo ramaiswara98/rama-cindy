00d2360a6f1abf78d1e3a6cdd115471741f11b3e
ramaiswara98 <ramaiswara098@gmail.com>
2023-11-30 02:49:54 +0800

    fix song issue

--- src/pages/Undangan.js
+++ src/pages/Undangan.js
@@ -1,18 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Kenalan from '../section/Kenalan/Kenalan'
 import OurStory from '../section/OurStory/OurStory'
 import Moment from '../section/Moment/Moment'
 import Acara from '../section/Acara/Acara'
 import Ucapan from '../section/Ucapan/Ucapan'
 import Closing from '../section/Closing/Closing'
-import Lagu from '../assets/lagu.mp3'
+import Lagu from '../assets/lagu.m4a'
 
 function Undangan() {
-
+  useEffect(() => {
+    document.getElementById('autoplay').play().catch((error) => {
+      document.addEventListener('touchstart', ()=>{
+        document.getElementById('autoplay').play().catch((error) => {
+          document.addEventListener('click', ()=> {
+            document.getElementById('autoplay').play()
+          })
+        })
+      },{once:true})
+    })
+  })
+  
   return (
     <div>
-      <audio autoPlay={'autoplay'} loop controls>
-        <source src={Lagu} type='audio/mpeg'/>
+      <audio autoPlay={'autoplay'} loop id='autoplay'>
+        <source src={Lagu} type='audio/mp4'/>
         Your Browser not support the audio element
       </audio>
         <Kenalan/>
